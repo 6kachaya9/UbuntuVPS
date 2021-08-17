84a43b2bdacf2eed372ae8b5ad7f37b8dfc7c024
6kachaya9 <86969936+6kachaya9@users.noreply.github.com>
2021-08-17 19:50:25 +0530

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -27,3 +27,27 @@ sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw enable
 
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt install qbittorrent-nox -y
+sudo adduser --system --group qbittorrent-nox
+sudo adduser kachaya qbittorrent-nox
+cd /etc/systemd/system
+echo '[Unit]' > qbittorrent-nox.service
+echo 'Description=qBittorrent Command Line Client' >> qbittorrent-nox.service
+echo 'After=network.target' >> qbittorrent-nox.service
+echo '[Service]' >> qbittorrent-nox.service
+echo '#Do not change to "simple"' >> qbittorrent-nox.service
+echo 'Type=forking' >> qbittorrent-nox.service
+echo 'User=qbittorrent-nox' >> qbittorrent-nox.service
+echo 'Group=qbittorrent-nox' >> qbittorrent-nox.service
+echo 'UMask=007' >> qbittorrent-nox.service
+echo 'ExecStart=/usr/bin/qbittorrent-nox -d --webui-port=8080' >> qbittorrent-nox.service
+echo 'Restart=on-failure' >> qbittorrent-nox.service
+echo '[Install]' >> qbittorrent-nox.service
+echo 'WantedBy=multi-user.target' >> qbittorrent-nox.service
+sudo systemctl start qbittorrent-nox
+sudo systemctl daemon-reload
+sudo systemctl enable qbittorrent-nox
+systemctl status qbittorrent-nox
+sudo ufw allow 8080
