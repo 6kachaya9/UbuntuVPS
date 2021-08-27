ab5a55dbbe8cfefa5073f6a21677a694d073fa37
6kachaya9 <86969936+6kachaya9@users.noreply.github.com>
2021-08-27 17:18:20 +0530

    Update ERPNext.sh

--- ERPNext.sh
+++ ERPNext.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+sudo passwd root
 sudo apt update
 sudo apt upgrade  -y
 sudo apt -y install vim libffi-dev python3-pip python3-dev  python3-testresources libssl-dev wkhtmltopdf
@@ -7,3 +9,11 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /us
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 sudo apt -y install gcc g++ make nodejs redis-server
+sudo apt -y install nginx
+sudo apt install mariadb-server
+sudo mysql -u root
+USE mysql;
+UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+UPDATE user SET authentication_string=password('your_password') WHERE user='root';
+FLUSH PRIVILEGES;
+EXIT;
